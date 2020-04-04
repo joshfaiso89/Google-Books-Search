cb37d28c049a0da49623c6531a9fa5a3840af925
Josh Faison <Joshfaiso@yahoo.com>
2020-04-04 13:48:26 -0400

    replacing h tags with card formatting

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
-import {ListItem} from '../List';
+import { ListItem } from '../List';
 
 function Book ({ title, authors, description, image, link }) {
     return(
         <div>
             <ListItem>
-            <h1>{title}</h1>
-            <h2>{authors}</h2>
-            <h2>{description}</h2>
-            <h2>{image}</h2>
-            <h2>{link}</h2>
+            <div class="card">
+    <img src={image} class="card-img-top" alt="..." style={{width:"20%"}}></img>
+    <div class="card-body">
+        <h5 class="card-title">{title}</h5>
+        <p class="card-text">{authors}}</p>
+        <p class="card-text">{description}</p>
+        <a href={link} class="btn btn-primary">View</a>
+        <a href="#" class="btn btn-primary">Save</a> 
+    </div>
+            </div>
             </ListItem>
         </div>
     )
 }
 
 export default Book;
+
+{/* <h1>{title}</h1>
+<h2>{authors}</h2>
+<h2>{description}</h2>
+<h2>{image}</h2>
+<h2>{link}</h2> */}
--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -1,49 +1,58 @@
 import React, { Component } from "react";
-import api from '../../utils/Api';
-import Book from '../Book';
-import { List } from '../List';
+import api from "../../utils/Api";
+import Book from "../Book";
+import { List } from "../List";
 
 class Container extends Component {
   state = {
     googleResults: [],
-    search: ''
-  }
+    search: ""
+  };
 
-  handleInputChange = (event) => {
+  handleInputChange = event => {
     const { name, value } = event.target;
     this.setState({
-      [name] : value   
-    })
-  }
+      [name]: value
+    });
+  };
 
   handleFormSubmit = () => {
     api.getBook(this.state.search).then(res => {
       this.setState({
         googleResults: res.data.items
-      })
-      console.log(res.data.items)
-    })
-
-  }
+      });
+      console.log(res.data.items);
+    });
+  };
 
   render() {
     return (
       <div>
-        <div className="jumbotron">Search for Books
-        <br></br>
-        <input type="text" name="search" value={this.state.search} onChange={this.handleInputChange}/><button onClick={this.handleFormSubmit}>Search</button>
+        <div className="jumbotron">
+          Search for Books
+          <br></br>
+          <input
+            type="text"
+            name="search"
+            value={this.state.search}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={this.handleFormSubmit}>Search</button>
         </div>
         <List>
-       {this.state.googleResults.map(displayBook => (
-         <Book title={displayBook.volumeInfo.title}/>
-       ))
-       }
+          {this.state.googleResults.map(displayBook => (
+            <Book
+              title={displayBook.volumeInfo.title}
+              authors={displayBook.volumeInfo.authors}
+              description={displayBook.volumeInfo.description}
+              image={displayBook.volumeInfo.imageLinks.thumbnail}
+              link={displayBook.volumeInfo.infoLink}
+            />
+          ))}
         </List>
       </div>
-      
     );
   }
-  
 }
 
 export default Container;
